64a7c0886f00511f2ebabecd142f133500895427
John Hiller <jdhiller@bellsouth.net>
2021-07-13 10:58:55 -0500

    initial commit

--- BestBuyBestPractices/DapperDepartmentRepository.cs
+++ BestBuyBestPractices/DapperDepartmentRepository.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
 namespace BestBuyBestPractices
 {
-    public class DapperDepartmentRepository
+    public class DapperDepartmentRepository : IDepartmentRepository
     {
-        public DapperDepartmentRepository()
+        private readonly IDbConnection _connection;
+
+        public DapperDepartmentRepository(IDbConnection connection)
         {
+            _connection = connection;
+        }
+
+        // Create GetAllDepartments Method -----
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            return _connection.Query<Department>("Select * From departments;");
+        }
+
+        // Create InsertDepartment Method -----
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("Insert into departments (Name) values (department name);",
+            new {departmentName = newDepartmentName});
         }
     }
 }
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +10,22 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Make sure config code is in Main Method -----
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+            var departments = repo.GetAllDepartments();
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+            }
+
         }
     }
 }
